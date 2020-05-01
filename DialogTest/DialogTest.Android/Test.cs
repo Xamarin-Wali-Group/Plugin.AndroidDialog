2e9a6103b87d2a43ecc6a77709b279f297d61dd9
武扬 李 <liwuyang1234@qq.com>
2020-05-01 10:03:38 +0800

    a

---
+++ DialogTest/DialogTest.Android/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.Android;
+
+namespace DialogTest.Droid
+{
+    public class Test: MapRenderer
+    {
+        public Test(Context context) :base(context){ }
+
+        protected override MarkerOptions CreateMarker(Pin pin)
+        {
+           
+            return base.CreateMarker(pin);
+        }
+    }
+
+}
